0dac8e5311e9af34cc1318140b9b43951f03c2e5
Patrick Stokes <Prstokes37@gmail.com>
2023-08-26 17:53:42 -0500

    added homepage, displayitem,routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,18 @@
-import { useState, useEffect } from 'react';
+import { Routes, Route } from "react-router-dom";
 import "./styles.css";
+import Homepage from './components/Homepage';
+import DisplayItem from "./components/DisplayItem";
 
 function App() {
-  const [items, setItems] = useState([]);
-
-  useEffect(() => {
-    const getAllItmes = async () => {
-      try{
-      const response = await fetch("https://fakestoreapi.com/products");
-      const result = await response.json();
-      console.log(result);
-      setItems(result);
-      } catch (error) {
-        console.log(error);
-      }
-    }
-    getAllItmes();
-  }, []);
-
-
+ 
   return (
-    <>
-    <header>My Online Shop
-      <label>Login
-      <input type="text" placeholder="login" />
-      </label>
-      <label>Password
-      <input type="text" placeholder="password" />
-      </label>
-    </header>
-      <div>
-        {items.map((item) => (
-          <div key={item.id} className='items-container'>
-            <h3>{item.title}</h3>
-            <h5>{item.price}</h5>
-            <img src={item.image} alt={item.title} />
-          </div>
-        ))}
-          
-      </div>
-    </>
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/:itemId" element={<DisplayItem />} />
+        
+      </Routes>
+    </div>
   )
 }
 
